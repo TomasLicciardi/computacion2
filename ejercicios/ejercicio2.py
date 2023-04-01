c92a1c5916d0e59cb0536f8bd50921b2efa18671
tomas <t.licciardi@alumno.um.edu.ar>
2023-04-01 18:50:01 -0300

    Modificando el main

--- ejercicios/ejercicio2.py
+++ ejercicios/ejercicio2.py
@@ -3,25 +3,26 @@ import sys
 
 parser = argparse.ArgumentParser(description='Repetir tantas veces el texto ingresado como indica el numero ingresado')
 
+def main():
 
+    parser.add_argument('--cadena', type=str, help='Ingresar texto para repetir')
+    parser.add_argument('--numero', type=int, help='Ingresar numero para repetir el texto')
 
-parser.add_argument('--cadena', type=str, help='Ingresar texto para repetir')
-parser.add_argument('--numero', type=int, help='Ingresar numero para repetir el texto')
+    args = parser.parse_args()
 
-args = parser.parse_args()
+    texto = args.cadena
+    numero = args.numero
 
-texto = args.cadena
-numero = args.numero
+    if numero <= 0:
+        print("Se ha introducido un numero incorrecto")
+        exit()
 
-if numero <= 0:
-    print("Se ha introducido un numero incorrecto")
-    exit()
+    cadena = []
 
-cadena = []
+    for i in range(numero):
+        cadena.append(texto)
 
-for i in range(numero):
-    cadena.append(texto)
+    cadena = " ".join(cadena)
 
-cadena = " ".join(cadena)
-
-print(cadena)
+if __name__== '__main__':
+    main()
