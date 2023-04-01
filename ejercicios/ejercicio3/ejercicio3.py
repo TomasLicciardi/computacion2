a5bbd88bd1751c7f24b074077e6d289aaabc3a8c
tomas <t.licciardi@alumno.um.edu.ar>
2023-04-01 20:55:40 -0300

    Actualizado, falta arreglar lo de mas de 1 espacio entre palabra

--- ejercicios/ejercicio3/ejercicio3.py
+++ ejercicios/ejercicio3/ejercicio3.py
@@ -11,12 +11,14 @@ def main():
     with open(args.archivo + '.txt', 'r') as archivo:
         lineas = archivo.readlines()
         num_lineas = len(lineas)
-        contenido = "".join(lineas)  
+        linea = []
+        for i in lineas:
+            linea.append(i.strip())
+        contenido = " ".join(linea)
         palabras = contenido.strip().split(" ")  
         longitud = "".join(palabras)
         promedio = len(longitud) / len(palabras)
-
-
+        
     print(f"El archivo tiene {len(palabras)} cantidad de palabras")
     print(f"El archivo tiene {num_lineas} cantidad de lineas")
     print(f"El promedio de la longitud de las palabras es {round(promedio)}")
