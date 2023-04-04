e7c47d9365024d6308e6d1fe0287e91ac95f4442
tomas <t.licciardi@alumno.um.edu.ar>
2023-04-04 16:54:30 -0300

    Modificando el ej5

--- ejercicios/ejercicio5.py
+++ ejercicios/ejercicio5.py
@@ -13,21 +13,23 @@ def main():
 
     args = parser.parse_args()
 
-    if args.numero:
+    if args.numero > 0:
         if args.fork:
             pid = os.fork()
             if pid > 0:
                 print(f"Soy el proceso padre. PID: {os.getpid()}")
-                print(f"La raíz cuadrada del número {args.numero} es {math.sqrt(args.numero)}")
+                resultado = round(math.sqrt(args.numero),2)
+                print(f"La raíz cuadrada del número {args.numero} es {resultado}")
             elif pid == 0:
                 print(f"Soy el proceso hijo. PID: {os.getpid()}")
-                print(f"La raíz cuadrada del número {-args.numero} es {cmath.sqrt(-args.numero)}")
+                resultado = str(round(math.sqrt(args.numero),2)) + 'j'
+                print(f"La raíz cuadrada del número {-args.numero} es {resultado}")
             else:
                 print("Se produjo un error al crear el proceso hijo y no se pudo crear un nuevo proceso")
         else:
             print("Falta el argumento '-f'")
     else:
-        print("Falta el argumento '-n'")
+        print("Ha ingresado mal el argumento")
         exit()
 
 if __name__ == '__main__':
